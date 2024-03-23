7b75bd12444660225af64dc2b616d31f14a48a23
Boonk3312 <theboonk7@hotmail.com>
2024-03-22 21:19:00 -0700

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -1,11 +1,9 @@
-/*
- *  init[] {
- *      @step: #define step1 = {
- *          static #define 18FF = "04 01 0E C5 B3 E7 AE 0E 12 2B D5 C9 A0 7F E5 D9 0A 55 E0 A9 BB";
- *          return convert@hex$get.stringdefine = /* 18FF #define statement get converted from hex to text*/ [18FF];
- *      }; 
- *  }
- */
+//  init[] {
+//       @step: #define step1 = {
+//           static #define 18FF = "04 01 0E C5 B3 E7 AE 0E 12 2B D5 C9 A0 7F E5 D9 0A 55 E0 A9 BB";
+//           return convert@hex$get.stringdefine = /* 18FF #define statement get converted from hex to text*/ [18FF];
+//       };
+//  }    
 
 // First, the constant value 18FF is defined as a hexadecimal string literal "04 01 0E C5 B3 E7 AE 0E 12 2B D5 C9 A0 7F E5 D9 0A 55 E0 A9 BB".
 // Next, the macro #define 18FF is used to define a symbolic name 18FF which represents the hexadecimal string literal.
