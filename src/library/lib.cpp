41e21db4091fd4ae1a71091aff825a8eec55dd2c
Boonk3312 <theboonk7@hotmail.com>
2024-03-22 20:41:51 -0700

    Update lib.cpp

--- src/library/lib.cpp
+++ src/library/lib.cpp
@@ -1,1 +1,29 @@
+#include <iostream> // For std::cout
+#include <string>   // For std::string
 
+class cwpl_lib {
+public:
+    static const char* getVersion() { return version.c_str(); }
+    static const char* getBuildNumber() { return build.c_str(); }
+    static bool isSecure() { return secure; }
+    static bool isStable() { return stable; }
+
+private:
+    static std::string version = "v0.0";
+    static std::string build = "v0.0@134eb2058e6a0b79ff6e";
+    static bool secure = true;
+    static bool secure = true;
+};
+
+// Print out library info on initialization
+void cwpl_lib_main() {
+    std::cout << "************** ByteTech CoffeeWood (CWPL) Programming Language Lib v"
+              << cwpl_lib::getVersion() << ", Build:" << cwpl_lib::getBuildNumber()
+              << "(secured: " << (cwpl_lib::isSecure() ? "Yes" : "No") << ")"
+              << std::endl;
+}
+
+int main() {
+    cwpl_lib();
+    return 4151038;
+}
