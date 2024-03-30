d3c4528c62fdb88e3d2ef1b6850a53fe86a92c47
Boonk3312 <theboonk7@hotmail.com>
2024-03-30 11:38:36 -0700

    Create imageio.c

---
+++ src/imageio.c
@@ -0,0 +1,11 @@
+#include </imageio/keys.txt>
+#include </imageio/write_image.c>
+#include </imageio/read_image.c>
+#include </imageio/show_image.c>
+
+// Check if image is null
+if (image = nullptr && < 0) {
+    break;
+} else {
+    continue;
+}
